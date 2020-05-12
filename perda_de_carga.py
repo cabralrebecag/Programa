8880dd8002e1af9c558fb1034d4b3dd5c33c4287
Rebeca Cabral <rebecacabral@MacBook-Pro-de-Rebeca.local>
2020-05-12 16:51:01 -0300

    Comentários nos programas para facilitar leitura

--- perda_de_carga.py
+++ perda_de_carga.py
@@ -7,6 +7,7 @@ Created on Mon Mar 23 08:00:46 2020
 
 import numpy as np
 import csv
+#dados de entrada
 H=76.2  #em mm
 D=10.75  #em mm
 At=H**2-25*((np.pi*D**2)/4)
@@ -22,34 +23,31 @@ from os.path import isfile, join
 import statistics
 
 def perda_carga(save_path):
-    onlyfiles = [f for f in listdir(save_path) if isfile(join(save_path, f))]
-    #del onlyfiles[1]
-    #del onlyfiles[2]
-    
+#codigo pra ler os arquivos e organiza-los em listas
+    onlyfiles = [f for f in listdir(save_path) if isfile(join(save_path, f))]    
     lista=[]
-
     p=[]
     re=[]
     ro=[]
-    vis=[]
-    
+    vis=[]  
     for aux in range (len(onlyfiles)):   
-        with open(save_path + "/" + onlyfiles[aux], newline='') as csvfile:
-        
+        with open(save_path + "/" + onlyfiles[aux], newline='') as csvfile:      
             spamreader = csv.reader(csvfile) 
             for linha in spamreader:
                 aux1=str(linha[0]).split()
-                lista.append(aux1)
-                
+                lista.append(aux1)               
             for i in lista:
-                p.append(i[23])
-        
+                p.append(i[23])       
     p=np.array(p,dtype=float)
+    
+#contas média da perda de carga
     
     media_p=(sum(p)/len(p))*100  #em pascal
     
     std_media_p=statistics.stdev(p)   #em mbar
     print("DP = " +str(media_p) + " +/- " + str(std_media_p))
+ 
+#contas média da densidade
     
     for i in lista:
         ro.append(i[9])
@@ -57,11 +55,15 @@ def perda_carga(save_path):
     ro=np.array(ro,dtype=float)    
     media_ro=(sum(ro)/len(ro))
     
+#contas média viscosidade
+    
     for i in lista:
         vis.append(i[11])
         
     vis=np.array(vis,dtype=float)    
     media_vis=(sum(vis)/len(vis))/1000
+
+#contas média reynolds
     
     for i in lista:
         re.append(i[19])
@@ -71,6 +73,7 @@ def perda_carga(save_path):
     
     v=(media_re*media_vis)/media_ro*Dh
     
+#colebrook
     
     fi=1
     f=fi
@@ -86,8 +89,11 @@ def perda_carga(save_path):
         if i==2: 
             print("deu i max")
             break
-        
+
+#altura manométrica        
     hl = f*H*v**2/Dh*2
+    
+# calculo de perda localizada
     
     k = 2*hl/v**2
     k = ("k = " +str(k))
